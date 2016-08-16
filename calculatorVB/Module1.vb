4c12e9d073614f55a4d33e6b7e93c1bb9416f1ee
Shotechs <shotechs@gmail.com>
2016-08-16 15:58:25 -0400

    n

---
+++ calculatorVB/Module1.vb
@@ -0,0 +1,54 @@
+﻿Module Module1
+
+    Sub Main()
+
+        shotechs.output()
+
+        Console.WriteLine("Type 1st number")
+        Dim x As Integer = Convert.ToInt32(Console.ReadLine())
+        Console.WriteLine("Type 2nd number")
+        Dim y As Integer = Convert.ToInt32(Console.ReadLine())
+        Console.WriteLine("///////////////////////")
+        Console.WriteLine("// Choose a option //")
+        Console.WriteLine("///////////////////////")
+        Console.WriteLine("1 - addition")
+        Console.WriteLine("2 - subtration ")
+        Console.WriteLine("3 - multification")
+        Console.WriteLine("4 - division")
+        Dim z As Integer = Convert.ToInt32(Console.ReadLine())
+
+        'userOption
+        Select Case z
+
+            Case 1
+                Console.WriteLine("///////////////////////")
+                Console.WriteLine("// Result //")
+                Console.WriteLine("///////////////////////")
+                Console.WriteLine(x + y)
+
+            Case 2
+                Console.WriteLine("///////////////////////")
+                Console.WriteLine("// Result //")
+                Console.WriteLine("///////////////////////")
+                Console.WriteLine(x - y)
+
+            Case 3
+                Console.WriteLine("///////////////////////")
+                Console.WriteLine("// Result //")
+                Console.WriteLine("///////////////////////")
+                Console.WriteLine(x * y)
+
+            Case 4
+                Console.WriteLine("///////////////////////")
+                Console.WriteLine("// Result //")
+                Console.WriteLine("///////////////////////")
+                Console.WriteLine(x / y)
+
+        End Select 'Ends Select Statements.
+
+        Console.ReadKey(True) 'Allows program to read in console window and used to exit on MenuExit.
+
+    End Sub
+
+End Module
+
---
+++ calculatorVB/shotechs.vb
@@ -0,0 +1,16 @@
+﻿Public Class shotechs
+
+    Public Shared Sub output()
+
+        Dim underline As String = "///////////////////////"
+        Dim shotechsLine As String = "// Shotechs@gmail.com //"
+
+        Console.WriteLine(underline)
+        Console.WriteLine(shotechsLine)
+        Console.WriteLine(underline)
+
+
+
+    End Sub
+
+End Class
